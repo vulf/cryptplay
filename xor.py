e72759208d80381012c4fea6e304744db82ce291
vulf <pranavsivvam@gmail.com>
2020-09-17 19:08:21 +0530

    xorrrr again without comments

--- xor.py
+++ xor.py
@@ -7,10 +7,8 @@ def xor(a, b):
     print("\n******\nbinary\n******")
     d1,d2 = 0,0
     if len(bin(a)) > len(bin(b)):
-        #d2 = len(bin(a)) - len(bin(b))
         d2 = len(bin(a)[2:])
     elif len(bin(b)) > len(bin(a)):
-        #d1 = len(bin(b)) - len(bin(a))
         d1 = len(bin(b)[2:])
     print(f"a = {bin(a)[2:].zfill(d1)}\nb = {bin(b)[2:].zfill(d2)}")
     print("\n*******\nXORing..\n*******")
