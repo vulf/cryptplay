d9b222d94a6632b5d4a20ed1b743de28edb3d8b1
vulf <pranavsivvam@gmail.com>
2020-09-19 15:12:17 +0530

    minor changes

--- pals/hex2b64.py
+++ pals/hex2b64.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+# hex to base64
 from base64 import b64encode, b64decode
 
-# hex to base64
-
-print('enter hex string: ', end='')
-msg = input()
+msg = input('enter hex string: ')
 msg_bytes = bytes.fromhex(msg)
 b64_enc = b64encode(msg_bytes)
 print(f'Base64 encoded data: {b64_enc.decode("utf-8")}')
