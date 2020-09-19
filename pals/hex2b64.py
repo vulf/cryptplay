b6d99caabe501882964b73bc11a613832f44f125
vulf <pranavsivvam@gmail.com>
2020-09-19 15:03:44 +0530

    completed first challenge in set 1

---
+++ pals/hex2b64.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from base64 import b64encode, b64decode
+
+# hex to base64
+
+print('enter hex string: ', end='')
+msg = input()
+msg_bytes = bytes.fromhex(msg)
+b64_enc = b64encode(msg_bytes)
+print(f'Base64 encoded data: {b64_enc.decode("utf-8")}')
+b64_dec = b64decode(b64_enc)
+print(f'Base64 decoded data: {b64_dec.decode("utf-8")}')
